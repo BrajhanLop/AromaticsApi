d0f5dd22a4aea6eda8c2dc766c8939734e2dc0b4
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-11-30 01:43:16 -0500

    Agregando variables de entorno

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,16 @@
 const { Sequelize, DataTypes  } = require('sequelize')
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const db = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'lopezs123',
-    database: 'Aromaticas'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    logging: false
 })
 
 module.exports = {db, DataTypes}
