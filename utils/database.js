f17c1b5cbf0e8aeb32f125c137ac118cb162e8a9
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-11-30 02:53:55 -0500

    var entorno

--- utils/database.js
+++ utils/database.js
@@ -9,7 +9,7 @@ const db = new Sequelize({
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+    database: process.env.DB || 'Aromaticas',
     logging: false
 })
 
